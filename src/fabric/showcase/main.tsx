1c3774ab7e95fd30db4b5bacb1c3c51cf30b35cd
BigBirdReturns <bigbirdreturns@proton.me>
2026-09-03 14:57:16 -0700

    Clamp every showcase stage to the owned cinematic frame

--- src/fabric/showcase/main.tsx
+++ src/fabric/showcase/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ShowcaseRoute } from "./ShowcaseRoute.js";
 import "./showcase-layout.css";
 import "./showcase-camera.css";
+import "./showcase-fit.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("No #root element");
