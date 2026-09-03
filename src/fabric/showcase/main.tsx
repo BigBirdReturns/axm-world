4355e0492d5e8a6c8b75224fd36385c0360e549d
BigBirdReturns <bigbirdreturns@proton.me>
2026-09-03 14:10:33 -0700

    Boot the Infinite Fabric showcase director

---
+++ src/fabric/showcase/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ShowcaseRoute } from "./ShowcaseRoute.js";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("No #root element");
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <ShowcaseRoute />
+  </React.StrictMode>,
+);
